955d6fa25317bce4d99da319e8710840783801b1
Alexis Costedoat <67351701+costeva@users.noreply.github.com>
2024-10-15 11:06:57 -0300

    cambios

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ const {
 } = require('./middlewares/error.handler');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //middleware que parsea el body a json
 app.use(express.json());
